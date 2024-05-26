65f4dcaf0f0a7fb8a5d455743538106b066bb912
kirtirajsinh <kirtiraj3738@gmail.com>
2024-05-26 13:20:46 +0530

    feat: Update layout to include NavBar component and improve session handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "@/components/common/SessionProvider";
 import { getServerSession } from "next-auth";
+import LoginButton from "@/components/auth/LoginButton";
+import NavBar from "@/components/NavBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +21,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <SessionProvider session={session}>
+          <div className="flex flex-col mt-12 mb-4 max-w-7xl mx-auto w-full px-6 lg:px-0  min-h-screen">
+            <NavBar />
+            {children}
+          </div>
+        </SessionProvider>
       </body>
     </html>
   );
