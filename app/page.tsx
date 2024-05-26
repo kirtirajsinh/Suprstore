ddd8ae6e3c013b6d0363e90aa66802d76f53a6e5
kirtirajsinh <kirtiraj3738@gmail.com>
2024-05-26 13:20:00 +0530

    refactor: Replace LoginButton with HeroSection on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import LoginButton from "@/components/auth/LoginButton";
+import HeroSection from "@/components/landingpage/HeroSection";
 import PostForm from "@/components/post/PostForm";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LoginButton />
-      <PostForm />
+      <HeroSection />
     </main>
   );
 }
