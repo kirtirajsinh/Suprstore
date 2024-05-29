6e813be4a630edf8d7b9233879f1093f5b28b071
kirtirajsinh <kirtiraj3738@gmail.com>
2024-05-29 23:21:28 +0530

    refactor: Update Post page component to import PostForm from correct file extension

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,6 +1,7 @@
 import PostForm from "@/components/post/PostForm";
 import React from "react";
 import { prisma } from "@/lib/db";
+import { auth } from "@/lib/auth";
 
 async function getGuilds() {
   const guilds = await prisma.guild.findMany({
@@ -14,6 +15,8 @@ async function getGuilds() {
 const Post = async () => {
   const guilds = await getGuilds();
   console.log(guilds);
+  const session = await auth();
+  console.log(session, "Session from Form");
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
