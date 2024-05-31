e25b422746d2557df6cecf36b7e0c78f85643877
kirtirajsinh <kirtiraj3738@gmail.com>
2024-05-31 11:16:19 +0530

    refactor: Update authHelpers to use TypeScript and improve JWT validation

--- lib/authHelpers.ts
+++ lib/authHelpers.ts
@@ -1,71 +1,77 @@
-import jwt, { JwtPayload, Secret, VerifyErrors } from "jsonwebtoken";
+import jwt, {
+  type JwtHeader,
+  type JwtPayload,
+  type Secret,
+  type VerifyErrors,
+} from "jsonwebtoken";
+
+// Type definition for the headers
+
+export interface DynamicKey {
+  key: Key;
+}
+
+export interface Key {
+  id: string;
+  publicKey: string;
+}
 
 export const getKey = (
-  headers,
+  headers: JwtHeader,
   callback: (err: Error | null, key?: Secret) => void
 ): void => {
-  console.log("calling getKey");
-
-  // Define the options for the fetch request
   const options = {
-    method: "GET",
+    method: "GET" as const, // Using 'as const' to narrow down the method type
     headers: {
       Authorization: `Bearer ${process.env.NEXT_DYNAMIC_BEARER_TOKEN}`,
     },
   };
 
-  // Perform the fetch request asynchronously
   fetch(
     `https://app.dynamicauth.com/api/v0/environments/${process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID}/keys`,
     options
   )
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
+    .then((response) => response.json())
+    .then((json: DynamicKey) => {
       const publicKey = json.key.publicKey;
       const pemPublicKey = Buffer.from(publicKey, "base64").toString("ascii");
       callback(null, pemPublicKey); // Pass the public key to the callback
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       console.error(err);
       callback(err); // Pass the error to the callback
     });
 };
 
-export const validateJWT = async (
-  token: string
-): Promise<JwtPayload | null> => {
+export const validateJWT = async (token: string): Promise<JwtPayload> => {
   try {
-    const decodedToken = await new Promise<JwtPayload | null>(
-      (resolve, reject) => {
-        jwt.verify(
-          token,
-          getKey,
-          { algorithms: ["RS256"] },
-          (
-            err: VerifyErrors | null,
-            decoded: string | JwtPayload | undefined
-          ) => {
-            console.log("decoded the jwt");
-            if (err) {
-              reject(err);
+    return await new Promise<JwtPayload>((resolve, reject) => {
+      jwt.verify(
+        token,
+        getKey,
+        { algorithms: ["RS256"] },
+        (
+          err: VerifyErrors | null,
+          decoded: JwtPayload | string | undefined
+        ) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (
+              typeof decoded === "object" &&
+              decoded !== null &&
+              "exp" in decoded
+            ) {
+              resolve(decoded);
             } else {
-              console.log("checking the type of decoded");
-              // Ensure that the decoded token is of type JwtPayload
-              if (typeof decoded === "object" && decoded !== null) {
-                resolve(decoded);
-              } else {
-                reject(new Error("Invalid token"));
-              }
+              reject(new Error("Invalid token"));
             }
           }
-        );
-      }
-    );
-    return decodedToken;
+        }
+      );
+    });
   } catch (error) {
     console.error("Invalid token:", error);
-    return null;
+    return Promise.reject(error);
   }
 };
